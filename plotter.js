b0673277841ef3bae90e02613231b85c555569c5
Ibnul Jahan <ijahan1@stuy.edu>
2018-04-13 09:07:57 -0400

    There is slight zoomability, and d3 now works offline

--- plotter.js
+++ plotter.js
@@ -1,4 +1,5 @@
 var pointInfo = document.getElementById("pointInfo");
+var zoomLevel = 1;
 
 var width = 700,
     height = 580;
@@ -11,7 +12,7 @@ var svg = d3.select('body')
 var g = svg.append('g');
 
 var albersProjection = d3.geoAlbers()
-    .scale(75000)
+    .scale(25000)
     .rotate([74.0060, 0])
     .center([0, 40.7128])
     .translate([width/2, height/2]);
@@ -44,18 +45,23 @@ console.log(albersProjection);
 
 
 //Nothing past here works yet
-/*
-var reSize = function(albersProjection, geoPath, g) {
+
+var reSize = function() {
     console.log(g);
+
+    //Make a new projection, but change only the scale
     albersProjection = d3.geoAlbers()
-	.scale(74000)
+	.scale(zoomLevel * 25000)
 	.rotate([74.0060, 0])
 	.center([0, 40.7128])
 	.translate([width/2, height/2]);
-    
     geoPath = d3.geoPath()
 	.projection(albersProjection);
 
+    //Remove the map
+    g.selectAll('path')
+	.remove()
+    //Redraw the map
     g.selectAll('path')
 	.data(boroughs_json.features)
 	.enter()
@@ -63,12 +69,28 @@ var reSize = function(albersProjection, geoPath, g) {
 	.attr('fill', '#2bf')
 	.attr('stroke', '#000')
 	.attr('d', geoPath);
+    
+    svg.selectAll("circle")
+	.remove()
+    svg.selectAll("circle")
+	.data(points.features).enter()
+	.append("circle")
+	.attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+	.attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+	.attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px")
+	.attr("fill", "red")
+	.on('click', function(d){
+	    console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
+	    document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + "";
+	});
+    
 	
 }
 
 var zoomMap = function(){
+    zoomLevel += 1;
     console.log("scale");
-    reSize(albersProjection, geoPath, g);
+    reSize();
 }
 
 
@@ -76,5 +98,3 @@ var zoomer = document.getElementById("zoom");
 //console.log(zoomer);
 zoomer.addEventListener("click", zoomMap );
 
-
-*/
