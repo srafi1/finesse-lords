5b935709aa22991b0e0135746d960e7abd9cea91
srafi1 <shakilrafi0@gmail.com>
2018-04-13 10:09:09 -0400

    zooooom

--- plotter.js
+++ plotter.js
@@ -36,55 +36,58 @@ svg.selectAll("circle")
     .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
     .attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px")
     .attr("fill", "red")
-    .on('click', function(d){
-	console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
-	document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + "";
-    });
+        .on('click', function(d){
+            console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
+            document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + " at a " + d.properties["Location Type"];
+        })
 
 console.log(albersProjection);
 
-
 //Nothing past here works yet
 
-var reSize = function() {
+var reSize = function(delta) {
+    zoomLevel += delta;
     console.log("Zoom Level: " + zoomLevel);
 
     //Make a new projection, but change only the scale
     albersProjection = d3.geoAlbers()
-	.scale(zoomLevel * 25000)
-	.rotate([74.0060, 0])
-	.center([0, 40.7128])
-	.translate([width/2, height/2]);
+        .scale(zoomLevel * 25000)
+        .rotate([74.0060, 0])
+        .center([0, 40.7128])
+        .translate([width/2, height/2]);
     geoPath = d3.geoPath()
-	.projection(albersProjection);
+        .projection(albersProjection);
 
     //Remove the map
-    g.selectAll('path')
-	.remove()
+    //g.selectAll('path')
+    //.remove()
     //Redraw the map
     g.selectAll('path')
-	.data(boroughs_json.features)
-	.enter()
-	.append('path')
-	.attr('fill', '#2bf')
-	.attr('stroke', '#000')
-	.attr('d', geoPath);
-    
+    //.data(boroughs_json.features)
+    //.enter()
+        .transition()
+        .duration(2000)
+    //.append('path')
+        .attr('fill', '#2bf')
+        .attr('stroke', '#000')
+        .attr('d', geoPath);
+
+    //svg.selectAll("circle")
+    //.remove()
     svg.selectAll("circle")
-	.remove()
-    svg.selectAll("circle")
-	.data(points.features).enter()
-	.append("circle")
-	.attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
-	.attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
-	.attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px")
-	.attr("fill", "red")
-	.on('click', function(d){
-	    console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
-	    document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + "";
-	});
-    
-	
+    //.data(points.features).enter()
+    //.append("circle")
+        .transition()
+        .duration(2000)
+        .attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+        .attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px")
+        .attr("fill", "red")
+        .on('click', function(d){
+            console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
+            document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + " at a " + d.properties["Location Type"];
+        })
+
 }
 
 var zoomMap = function(){
@@ -93,8 +96,7 @@ var zoomMap = function(){
     reSize();
 }
 
-
 var zoomer = document.getElementById("zoom");
-//console.log(zoomer);
-zoomer.addEventListener("click", zoomMap );
-
+zoomer.addEventListener("click", () => reSize(1) );
+var unzoomer = document.getElementById("unzoom");
+unzoomer.addEventListener("click", () => reSize(-1) );
