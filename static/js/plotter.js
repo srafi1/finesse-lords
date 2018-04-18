9a66fd12e73d1c71a42db9191e7fb38855fdd311
srafi1 <shakilrafi0@gmail.com>
2018-04-17 22:28:21 -0400

    zoomstuff

--- static/js/plotter.js
+++ static/js/plotter.js
@@ -2,85 +2,123 @@ var pointInfo = document.getElementById("pointInfo");
 var zoomLevel = 1;
 
 var width = 700,
-	height = 580;
+    height = 580;
+var panX = width/2,
+    panY = height/2,
+    move = false;
 
-var svg = d3.select('body')
-	.append('svg')
-	.attr('width', width)
-	.attr('height', height);
+var svg = d3.select('svg')
+    .attr('width', width)
+    .attr('height', height);
 
 var g = svg.append('g');
 
 var albersProjection = d3.geoAlbers()
-	.scale(25000)
-	.rotate([74.0060, 0])
-	.center([0, 40.7128])
-	.translate([width/2, height/2]);
+    .scale(60000 + 10000*(zoomLevel)**2)
+    .rotate([74.0060, 0])
+    .center([0, 40.7128])
+    .translate([panX, panY]);
 
 var geoPath = d3.geoPath().projection(albersProjection);
 
 g.selectAll('path')
-	.data(boroughs_json.features)
-	.enter()
-	.append('path')
-	.attr('fill', '#2bf')
-	.attr('stroke', '#000')
-	.attr('d', geoPath);
-
+    .data(boroughs_json.features)
+    .enter()
+    .append('path')
+    .attr('fill', '#2bf')
+    .attr('stroke', '#000')
+    .attr('d', geoPath);
 
 svg.selectAll("circle")
-	.data(points.features).enter()
-	.append("circle")
-	.attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
-	.attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
-	.attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px")
-	.attr("fill", "red")
-	.on('click', function(d){
-		console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
-		document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + " at a " + d.properties["Location Type"];
-	})
+    .data(points.features).enter()
+    .append("circle")
+    .attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+    .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+    .attr("r", (d) => Math.min(d.properties.num_calls/20, 5) + "px")
+    .attr("fill", "red")
+    .on('click', function(d){
+        console.log(d.properties.num_calls + " calls were made in complaint at " + albersProjection(d.geometry.coordinates)[0] + " , " + albersProjection(d.geometry.coordinates) [1] );
+        document.getElementById("pointInfo").innerHTML = "There were " + d.properties.num_calls + " noise complaint calls made at latitude " + d.geometry.coordinates[0] + " and longitude " + d.geometry.coordinates[1] + " at a " + d.properties["Location Type"];
+    })
 
 var resize = function(delta) {
-	zoomLevel += delta;
-	console.log("Zoom Level: " + zoomLevel);
+    zoomLevel += delta;
+    if (zoomLevel < -1) {
+        zoomLevel -= delta;
+        console.log("Can't zoom out that much");
+        return;
+    } else if (zoomLevel > 20) {
+        zoomLevel -= delta;
+        console.log("Can't zoom in that much");
+        return;
+    }
+    console.log("Zoom Level: " + zoomLevel);
 
-	//Make a new projection, but change only the scale
-	albersProjection.scale(zoomLevel * 25000)
+    var zoomDelta = 10000*(zoomLevel)**2;
+    if (zoomLevel < 0) {
+        zoomDelta *= -1;
+    }
+    albersProjection.scale(60000 + zoomDelta)
 
-	geoPath.projection(albersProjection);
+    g.selectAll('path')
+        .transition()
+        .duration(1000)
+        .attr('d', geoPath);
 
-	g.selectAll('path')
-		.transition()
-		.duration(1000)
-		.attr('d', geoPath);
-
-	svg.selectAll("circle")
-		.transition()
-		.duration(1000)
-		.attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
-		.attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
-		.attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px");
+    svg.selectAll("circle")
+        .transition()
+        .duration(1000)
+        .attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+        .attr("r", (d) => Math.min(d.properties.num_calls/(30 - zoomLevel*2), 5 + zoomLevel*2) + "px");
 }
 
+//doesn't work properly
 var moveTo = function(x, y) {
-	albersProjection.translate([x, y]);
-	geoPath.projection(albersProjection);
+    albersProjection.translate([x, y]);
+    geoPath.projection(albersProjection);
 
-	g.selectAll('path')
-		.transition()
-		.duration(1000)
-		.attr('d', geoPath);
+    g.selectAll('path')
+        .transition()
+        .duration(1000)
+        .attr('d', geoPath);
 
-	svg.selectAll("circle")
-		.transition()
-		.duration(1000)
-		.attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
-		.attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
-		.attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px");
+    svg.selectAll("circle")
+        .transition()
+        .duration(1000)
+        .attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+        .attr("r", (d) => Math.min(d.properties.num_calls/10, 10) + "px");
 }
 
-var zoomer = document.getElementById("zoom");
-zoomer.addEventListener("click", () => resize(1) );
-var unzoomer = document.getElementById("unzoom");
-unzoomer.addEventListener("click", () => resize(-1) );
-svg.on('click', () => moveTo(d3.event.offsetX, d3.event.offsetY));
+d3.select("#zoom").on('click', () => resize(1));
+d3.select("#unzoom").on('click', () => resize(-1));
+d3.select('body').on('keydown', () => {
+    switch(d3.event.keyCode) {
+        case 37:
+            //arrow left
+            panX += 20;
+            break;
+        case 38:
+            //arrow up
+            panY += 20;
+            break;
+        case 39:
+            //arrow right
+            panX -= 20;
+            break;
+        case 40:
+            //arrow down
+            panY -= 20;
+            break;
+    }
+
+    albersProjection.translate([panX, panY]);
+
+    g.selectAll('path')
+        .attr('d', geoPath);
+
+    svg.selectAll("circle")
+        .attr("cx", (d) => albersProjection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => albersProjection(d.geometry.coordinates)[1])
+});
